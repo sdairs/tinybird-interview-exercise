68b0ebc2ac2f4b82d4f61e81b8f2c1da056120ff
sdairs <me@alasdairb.com>
2022-05-09 19:22:33 +0100

    env vars for method & event count. Error handling.

--- data_gen/data_gen.py
+++ data_gen/data_gen.py
@@ -2,13 +2,30 @@ from kafka_producer import ACMEKafkaProducer
 from file_writer import ACMEFileWriter
 from datetime import datetime
 import parcel_shipping_generator as ps_gen
+from dotenv import load_dotenv
+import os
+import sys
 
-PRODUCE_TO_KAFKA = True
-WRITE_TO_FILE = False
+load_dotenv()
 
-EVENT_COUNT = 10000
+METHOD = os.environ.get('METHOD')
+EVENT_COUNT = os.environ.get('EVENT_COUNT')
 
-if PRODUCE_TO_KAFKA:
+if EVENT_COUNT is None:
+    EVENT_COUNT = 10000
+else:
+    try:
+        EVENT_COUNT = int(EVENT_COUNT)
+        if EVENT_COUNT < 0:
+            raise RuntimeError
+    except:
+        print('ERROR: Ensure EVENT_COUNT is a valid positive integer')
+        sys.exit(1)
+if METHOD is None:
+    raise RuntimeError(
+        'No METHOD specified. You must configure a .env file with a valid METHOD of either "kafka" or "s3".')
+
+if METHOD == 'kafka':
     producer = ACMEKafkaProducer()
     start_time = datetime.now()
     for x in ps_gen.generate_parcel_data(EVENT_COUNT):
@@ -18,7 +35,7 @@ if PRODUCE_TO_KAFKA:
     print('Took {end_time} seconds'.format(
         end_time=(end_time-start_time).total_seconds()))
 
-if WRITE_TO_FILE:
+elif METHOD == 's3':
     writer = ACMEFileWriter()
     start_time = datetime.now()
     print('Generating Data')
@@ -29,3 +46,6 @@ if WRITE_TO_FILE:
     end_time = datetime.now()
     print('Took {end_time} seconds'.format(
         end_time=(end_time-start_time).total_seconds()))
+
+else:
+    print(f'METHOD value was not recognised: {METHOD}')
